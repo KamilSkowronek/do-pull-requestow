64e2d6e765bbe8c50f3ecfda144ed4c2d8e019f5
Bartłomiej Paziewski <bartuuus94@gmail.com>
2015-03-15 17:08:12 +0100

    Update DefaultController.php

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -7,6 +7,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
+{
+
+    /*
+     * PRZYKŁADOWA AKCJA
+     */
+
 
     /**
      * @Route("/", name="homepage")
@@ -16,233 +22,25 @@ class DefaultController extends Controller
         return $this->render('default/index.html.twig');
     }
 
-    /*
-     * A
-     */
-    /**
-    * @Route("/czlek-zdechnie.html", name="czlek-zdechnie")
+   /*
+    * <-----------------------------KOKSU-----------------------------> (ROBISZ 2 ENTERY POD SPODEM I WRZUCASZ AKCJE)
     */
-    public function czlekZdechnieAction()
-    {
-        return $this->render('default/czlek-zdechnie.html.twig');
-    }
-
-    /*
-     * B
-     */
-    /**
-    * @Route("/bogatemu-to.html", name="bogatemu-to")
-    */
-    public function bogatemuToAction()
-    {
-        return $this->render('default/bogatemu-to.html.twig');
-    }
-    /*
-     * C
-     */
-
-    /**
-     * @Route("/D.html", name="D")
-     */
-    public function DAction()
-    {
-        return $this->render('default/D.html.twig');
-    }
-
-    /*
-     * E
-     */
     
-    
-    /**
-    * @Route("/bez-matki.html", name="bez matki")
-    */
-    public function bezmatkiAction()
-    {
-    return $this->render('default/bez-matki.html.twig');
-    }
-    
-    
-    /*
-     * F
-     */
-    /**
- * @Route("/Aby-do-wiosny.html", name="Aby-do-wiosny")
- */
-    public function AbydowiosnyAction()
-{
-    return $this->render('default/Aby-do-wiosny.html.twig');
-}
-
-    /*
-     * G
-     */
-
-    /**
-    * @Route("/stare-wino.html", name="stare-wino")
-    */
-    public function stareWinoAction()
-    {
-        return $this->render('default/stare-wino.html.twig');
-    }
-
-    /*
-     * H
-     */
-
-    /**
-    * @Route("/suchy-marzec.html", name="suchy-marzec")
-    */
-    public function suchyMarzecAction()
-    {
-        return $this->render('default/suchy-marzec.html.twig');
-    }
-
-    /*
-     * I
-     */
-    
-    /**
-    * @Route("/im-dab-starszy.html", name="im-dab-starszy")
-    */
-    public function ImDabStarszyAction()
-    {
-    return $this->render('default/im-dab-starszy.html.twig');
-    }   
-
-    /*
-     * J
-     */
-
-    /*
-     * K
-     */
-
-    /*
-     * L
-     */
-
-    /*
-     * M
-     */
-
-    /*
-     * N
-     */
-
-    /*
-     * O
-     */
-    
-    /**
-     * @Route("/glupota-ludzka.html", name="glupota-ludzka")
-     */
-    public function glupotaludzkaAction()
-    {
-    return $this->render('default/glupota-ludzka.html.twig');
-    }
-        
-    /*
-     * P
-     */
-
-    /**
-     * @Route("/szewc-bez.html", name="szewc-bez")
-     */
-     public function szewcbezAction()
-     {
-     return $this->render('default/szewc-bez.html.twig');
-     } 
-     
-    /*
-     * Q
-     */
-
-    /*
-     * R
-     */
-
-    /**
-     * @Route("/co-za-duzo.html", name="co-za-duzo")
-     */
-      public function coZaDuzoAction()
-     {
-     return $this->render('default/co-za-duzo.html.twig');
-     } 
-      
-    /*
-     * S
-     */
-    /**
-     * @Route("/jak-sobie.html", name="jak-sobie")
-     */
-    public function jakSobieAction()
-    {
-        return $this->render('default/jak-sobie.html.twig');
-    }
-    /*
-     * T
-     */
-
-    /*
-     * U
-     */
-
-    /*
-     * V
-     */
-
-    /**
-     * @Route("/gdzie-sie-z-checia.html", name="gdzie-sie-z-checia")
-     */
-    public function gdzieSieZCheciaAction()
-    {
-        return $this->render('default/gdzie-sie-z-checia.html.twig');
-    }
-
-    /*
-     * W
-     */
-
-    /*
-     * X
-     */
-    
-    /**
-    * @Route("/regula-prosta.html", name="regula-prosta")
-    */
-    public function regulaprostaAction()
-    {
-        return $this->render('default/regula-prosta.html.twig');
-    }
-
-    /*
-     * Y
-     */
-    
-    /**
- * @Route("/kto-pod-kim-dolki.html", name="kto-pod-kim-dolki")
- */
-public function ktopodkimdolkiAction()
-{
-    return $this->render('default/kto-pod-kim-dolki.html.twig');
-}
-    
- /**
+   /**
      * @Route("/male-zyski.html", name="male-zyski")
      */
     public function malezyskiAction()
     {
         return $this->render('default/male-zyski.html.twig');
     }
-    
-    
-    
-    
 
     /*
-     * Z
-     */
+    * <-----------------------------AREK-----------------------------> (ROBISZ 2 ENTERY POD SPODEM I WRZUCASZ AKCJE)
+    */
 
+
+    /*
+    * <-----------------------------BARTUŚ----------------------------> (ROBISZ 2 ENTERY POD SPODEM I WRZUCASZ AKCJE)
+    */
 
 }
